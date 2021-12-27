c6c2884363f65dcf0a77869747efe8efc7edbefe
vane79 <git1_git2_git3>
2021-12-27 13:04:05 +0700

    Fix emergent bugs in selenium_fetch_stats.py

--- selenium_fetch_stats.py
+++ selenium_fetch_stats.py
@@ -1,6 +1,7 @@
 from creds import login, pwd
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
 url = 'https://admin.keywording.pro/'
@@ -8,21 +9,22 @@ url = 'https://admin.keywording.pro/'
 
 def selenium():
     ffox_options = Options()
-    ffox_options.headless = True
+    # ffox_options.headless = True
     sel = webdriver.Firefox(options=ffox_options)
     sel.implicitly_wait(5)
     sel.get(url)
-    sel.find_element_by_id('loginform-email').send_keys(login)
-    sel.find_element_by_id('loginform-password').send_keys(pwd)
-    sel.find_element_by_name('login-button').click()
+    sel.find_element(By.CSS_SELECTOR, '#loginform-email').send_keys(login)
+    sel.find_element(By.CSS_SELECTOR, '#loginform-password').send_keys(pwd)
+    sel.find_element(By.CSS_SELECTOR, '.col-xs-4').click()
     sleep(1)
-    sel.find_element_by_css_selector('li.treeview:nth-child(3)').click()
-    sel.find_element_by_css_selector('li.treeview:nth-child(3)').click()
-    sel.find_element_by_css_selector('.fa-line-chart').click()
-    photos = sel.find_element_by_xpath(
-        '/html/body/div[1]/div/section[2]/div/div/div[2]/div[3]/table/tfoot/tr/td[3]').text
-    videos = sel.find_element_by_xpath(
-        '/html/body/div[1]/div/section[2]/div/div/div[2]/div[3]/table/tfoot/tr/td[4]').text
+    sel.find_element(By.CSS_SELECTOR, 'li.treeview:nth-child(3)').click()
+    sel.find_element(By.CSS_SELECTOR, 'li.treeview:nth-child(3)').click()
+    sel.find_element(By.CSS_SELECTOR, '.fa-line-chart').click()
+    # sel.find_element(By.CSS_SELECTOR, '.fa-line-chart').click()
+    photos = sel.find_element(By.CSS_SELECTOR,
+                              '.kv-table-footer > td:nth-child(3)').text
+    videos = sel.find_element(By.CSS_SELECTOR,
+                              '.kv-table-footer > td:nth-child(4)').text
     sel.quit()
     return photos, videos
 
